9d1730925c38887fdea2b058b4810cfc6f4160c5
DWhirlpool <ludaniel20110@outlook.com>
2022-04-26 17:42:13 +0800

    added copy tailwind classes button

---
+++ assets/js/copy.js
@@ -0,0 +1,85 @@
+function copytw1()
+{
+    var tw1 = "bg-gradient-to-r from-red-50 to-red-900"
+    navigator.clipboard.writeText(tw1)
+}
+function copytw2()
+{
+    var tw2 = "bg-gradient-to-r from-orange-50 to-orange-900"
+    navigator.clipboard.writeText(tw2)
+}
+function copytw3()
+{
+    var tw3 = "bg-gradient-to-r from-amber-50 to-amber-900"
+    navigator.clipboard.writeText(tw3)
+}
+function copytw4()
+{
+    var tw4 = "bg-gradient-to-r from-yellow-50 to-yellow-900"
+    navigator.clipboard.writeText(tw4)
+}
+function copytw5()
+{
+    var tw5 = "bg-gradient-to-r from-lime-50 to-lime-900"
+    navigator.clipboard.writeText(tw5)
+}
+function copytw6()
+{
+    var tw6 = "bg-gradient-to-r from-green-50 to-green-900"
+    navigator.clipboard.writeText(tw6)
+}
+function copytw7()
+{
+    var tw7 = "bg-gradient-to-r from-emerald-50 to-emerald-900"
+    navigator.clipboard.writeText(tw7)
+}
+function copytw8()
+{
+    var tw8 = "bg-gradient-to-r from-teal-50 to-teal-900"
+    navigator.clipboard.writeText(tw8)
+}
+function copytw9()
+{
+    var tw9 = "bg-gradient-to-r from-cyan-50 to-cyan-900"
+    navigator.clipboard.writeText(tw9)
+}
+function copytw10()
+{
+    var tw10 = "bg-gradient-to-r from-sky-50 to-sky-900"
+    navigator.clipboard.writeText(tw10)
+}
+function copytw11()
+{
+    var tw11 = "bg-gradient-to-r from-blue-50 to-blue-900"
+    navigator.clipboard.writeText(tw11)
+}
+function copytw12()
+{
+    var tw12 = "bg-gradient-to-r from-indigo-50 to-indigo-900"
+    navigator.clipboard.writeText(tw12)
+}
+function copytw13()
+{
+    var tw13 = "bg-gradient-to-r from-violet-50 to-violet-900"
+    navigator.clipboard.writeText(tw13)
+}
+function copytw14()
+{
+    var tw14 = "bg-gradient-to-r from-purple-50 to-purple-900"
+    navigator.clipboard.writeText(tw14)
+}
+function copytw15()
+{
+    var tw15 = "bg-gradient-to-r from-fuchisa-50 to-fuchisa-900"
+    navigator.clipboard.writeText(tw15)
+}
+function copytw16()
+{
+    var tw16 = "bg-gradient-to-r from-pink-50 to-pink-900"
+    navigator.clipboard.writeText(tw16)
+}
+function copytw17()
+{
+    var tw17 = "bg-gradient-to-r from-rose-50 to-rose-900"
+    navigator.clipboard.writeText(tw17)
+}
