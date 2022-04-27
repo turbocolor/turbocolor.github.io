a21f7e51fe878b6287dfecf9c586ea979254ba7c
Daniel Lu <95860724+DWhirlpool@users.noreply.github.com>
2022-04-27 10:02:27 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
 function source(){
   /* Only For Chronium-Based Browsers */
   window.location.replace("view-source://turbocolor.github.io/")
